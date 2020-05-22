f4af84f8542305d16d77afe2e07552b5f25ce9c9
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-22 15:29:49 -0400

    Made initial localhost mongodb connection

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,14 +10,47 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect("mongodb://localhost:27017/todoListDB", options);
+
+// Schema
+const itemsSchema = {
+  name: String
+};
+
+// Mongoose Model
+const Item = mongoose.model("Item", itemsSchema);
+
+// Default items
+const item1 = new Item({
+  name: "Welcome to your To Do List!"
+});
+const item2 = new Item({
+  name: "Hit the + to add a new item!"
+});
+const item3 = new Item({
+  name: "<- Hit the checkbox to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Added default stuff to DB");
+  }
+});
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
