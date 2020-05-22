5b044eb49b4b6e8e0e384d3f7db50e6ad773d4ff
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-22 15:46:51 -0400

    rendering db items to app

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 const options = {
@@ -38,23 +40,28 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Added default stuff to DB");
-  }
+app.get("/", function (req, res) {
+  Item.find({}, (err, foundItems) => {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Added default stuff to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    };
+  });
 });
 
-app.get("/", function(req, res) {
-
-
-
-  res.render("list", {listTitle: "Today", newListItems: items});
-
-});
-
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
   const item = req.body.newItem;
 
@@ -67,14 +74,17 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
