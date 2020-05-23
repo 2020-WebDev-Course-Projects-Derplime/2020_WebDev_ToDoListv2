aa85c899955eaad8baa8be0977bf1e7f9fc27ad7
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-23 00:43:09 -0400

    add items to custom todo lists

--- app.js
+++ app.js
@@ -98,14 +98,26 @@ app.get("/:listName", function (req, res) {
 app.post("/", function (req, res) {
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName
   });
 
-  item.save();
-
-  res.redirect("/");
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, (err, foundList) => {
+      if (err) {
+        console.error(err);
+      } else {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect(`/${listName}`);
+      }
+    });
+  }
 });
 
 app.post("/delete", function (req, res) {
